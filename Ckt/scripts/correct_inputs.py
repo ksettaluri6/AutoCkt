3ca0b0452c889e63793d66e76d8b7c2e26d9dc0b
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2023-06-07 16:50:14 -0700

    correct path for inputs

--- Ckt/scripts/correct_inputs.py
+++ Ckt/scripts/correct_inputs.py
@@ -1,27 +1,33 @@
 import os
 import re
 
+
 def update_file(fname, path_to_model):
-    print("changing "+ fname)
-    with open(fname, 'r') as f:
+    print("changing " + fname)
+    with open(fname, "r") as f:
         lines = f.readlines()
 
     for line_num, line in enumerate(lines):
-        if '.include' in line:
-            regex = re.compile("\.include\s*\"(.*?45nm\_bulk\.txt)\"")
+        if ".include" in line:
+            regex = re.compile('\.include\s*"(.*?45nm\_bulk\.txt)"')
             found = regex.search(line)
             if found:
                 lines[line_num] = lines[line_num].replace(found.group(1), path_to_model)
 
-    with open(fname, 'w') as f:
+    with open(fname, "w") as f:
         f.writelines(lines)
         f.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cur_fpath = os.path.realpath(__file__)
     parent_path = os.path.abspath(os.path.join(cur_fpath, os.pardir))
-    netlist_path = os.path.join(parent_path, 'netlist')
-    spice_model = os.path.join(parent_path, 'spice_models/45nm_bulk.txt')
+    netlist_path = os.path.join(
+        parent_path, "../eval_engines/ngspice/ngspice_inputs/netlist"
+    )
+    spice_model = os.path.join(
+        parent_path, "../eval_engines/ngspice/ngspice_inputs/spice_models/45nm_bulk.txt"
+    )
 
     for root, dirs, files in os.walk(netlist_path):
         for f in files:
